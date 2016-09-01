9a35905dad0ae03e7bd983a1bebaaea8b2dfc5db
KyzerSose <matt@theshaeffers.com>
2016-09-01 13:57:39 -0500

    Changing xml to show same lists on all three views

--- app/src/main/java/com/theshaeffers/musicplayer/MainActivity.java
+++ app/src/main/java/com/theshaeffers/musicplayer/MainActivity.java
@@ -20,31 +20,21 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, ArtistActivity.class));
             }
         });
-
-        TextView songsView = (TextView) findViewById(R.id.songs_textview);
-        songsView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, SongsActivity.class));
-            }
-        });
-
-        TextView playlistsView = (TextView) findViewById(R.id.playlists_textview);
-        playlistsView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, PlaylistActivity.class));
-            }
-        });
-
-        TextView recentView = (TextView) findViewById(R.id.recent_textview);
-        recentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, MainActivity.class));
-            }
-        });
-
-
+//
+//        TextView songsView = (TextView) findViewById(R.id.songs_textview);
+//        songsView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(MainActivity.this, SongsActivity.class));
+//            }
+//        });
+//
+//        TextView playlistsView = (TextView) findViewById(R.id.playlists_textview);
+//        playlistsView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(MainActivity.this, PlaylistActivity.class));
+//            }
+//        });
     }
 }
