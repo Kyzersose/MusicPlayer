814f6a5d94514ca6c00834e6b8c4ffabdb90bec4
KyzerSose <matt@theshaeffers.com>
2016-09-01 14:53:41 -0500

    Songs java/xml completed.

--- app/src/main/java/com/theshaeffers/musicplayer/SongsActivity.java
+++ app/src/main/java/com/theshaeffers/musicplayer/SongsActivity.java
@@ -1,7 +1,10 @@
 package com.theshaeffers.musicplayer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SongsActivity extends AppCompatActivity {
 
@@ -9,5 +12,61 @@ public class SongsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_songs);
+
+        TextView songsView1 = (TextView) findViewById(R.id.song_choice_view1);
+        songsView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SongsActivity.this, PlayActivity.class));
+            }
+        });
+
+        TextView songsView2 = (TextView) findViewById(R.id.song_choice_view2);
+        songsView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SongsActivity.this, PlayActivity.class));
+            }
+        });
+
+        TextView songsView3 = (TextView) findViewById(R.id.song_choice_view3);
+        songsView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SongsActivity.this, PlayActivity.class));
+            }
+        });
+
+        TextView songsView4 = (TextView) findViewById(R.id.song_choice_view4);
+        songsView4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SongsActivity.this, PlayActivity.class));
+            }
+        });
+
+        TextView songsView5 = (TextView) findViewById(R.id.song_choice_view5);
+        songsView5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SongsActivity.this, PlayActivity.class));
+            }
+        });
+
+        TextView songsView6 = (TextView) findViewById(R.id.song_choice_view6);
+        songsView6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SongsActivity.this, PlayActivity.class));
+            }
+        });
+
+        TextView songsView7 = (TextView) findViewById(R.id.song_choice_view7);
+        songsView7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SongsActivity.this, PlayActivity.class));
+            }
+        });
     }
 }
